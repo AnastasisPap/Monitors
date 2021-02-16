9f1b426d29ffe3523992f7fe63c6922d30af5653
Anastasis <anastasis.papapanagiotou@gmail.com>
2021-02-16 20:05:10 +0200

    Finished target monitor

--- Monitors/target_monitor.py
+++ Monitors/target_monitor.py
@@ -3,7 +3,12 @@ from send_webhook import send_webhook
 from time import sleep
 import requests
 import json
+from global_functions import *
 file_name = 'target_logs.txt'
+
+
+def get_title(product):
+    return product["title"]
 
 
 def check_availability(product):
@@ -13,17 +18,46 @@ def check_availability(product):
     return True
 
 
+def get_price(product):
+    price = product["price"]["formatted_current_price"]
+    return price
+
+
+def get_image(product):
+    images = product["images"][0]
+    base_url = images["base_url"]
+    primary = images["primary"]
+    return base_url + primary
+
+
+def get_url(product):
+    return "https://www.target.com" + product["url"]
+
+
 def main(product_id):
-    s = requests.get("https://www.target.com/")
-    key = s.cookies['visitorId']
-    url = f'https://redsky.target.com/v2/plp/collection/{product_id}?key={key}&pricing_store_id=2776'
-    res = requests.get(url).json()
-    product = res["search_response"]["items"]["Item"][0]
-    isAvailable = check_availability(product)
-    print(isAvailable)
+    hasSent = False
+    while True:
+        s = requests.get("https://www.target.com/")
+        key = s.cookies['visitorId']
+        url = f'https://redsky.target.com/v2/plp/collection/{product_id}?key={key}&pricing_store_id=2776'
+        res = requests.get(url).json()
+        product = res["search_response"]["items"]["Item"][0]
+        isAvailable = check_availability(product)
+
+        if not isAvailable and hasSent:
+            hasSent = False
+
+        if isAvailable and not hasSent:
+            append_to_logs(file_name, 'Found item in stock\n')
+            hasSent = True
+            title = get_title(product)
+            price = get_price(product)
+            image_url = get_image(product)
+            url = get_url(product)
+            send_webhook(url, "Target: item in stock", title, image_url, price, product_id)
+
+        sleep(2)
 
 
 if __name__ == '__main__':
-    main(81114595)
-
-    store_id = 1865
+    main(81114474)
