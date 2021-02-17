b1436fab67eb510afadb70c70a5e7087a368afe5
AnastasisPap <anastasis.papapanagiotou@gmail.com>
2021-02-17 07:57:36 +0000

    Changes to very monitor

--- Monitors/very_monitor.py
+++ Monitors/very_monitor.py
@@ -9,26 +9,42 @@ file_name = 'very_logs.txt'
 
 def check_availability(products, keyword):
     for i, product in enumerate(products):
-        title = product.find('span', class_='productBrandDesc').text.replace("\n", "")
-        if keyword in title:
-            return i, True, title
+        try:
+            title = product.find('span', class_='productBrandDesc').text.replace("\n", "")
+            if keyword in title.lower():
+                return i, True, title
+        except:
+            append_to_logs(file_name, f'Error finding product description {get_time()}\n')
+            append_to_logs(file_name, f'{product}\n')
 
     return None, False, None
 
 
 def get_image_url(soup):
-    image_url = soup.find('img')['src']
-    return image_url
+    try:
+        image_url = soup.find('img')['src']
+        return image_url
+    except:
+        append_to_logs(file_name, f'Error finding image {get_time()}\n')
+        append_to_logs(file_name, f'{soup}\n')
 
 
 def get_price(soup):
-    price = soup.find('dd', class_='productPrice').text.replace("\n", "")
-    return price
+    try:
+        price = soup.find('dd', class_='productPrice').text.replace("\n", "")
+        return price
+    except:
+        append_to_logs(file_name, f'Error finding price {get_time()}\n')
+        append_to_logs(file_name, f'{soup}\n')
 
 
 def get_link(soup):
-    url = soup.find('a', class_='productMainImage')['href']
-    return url
+    try:
+        url = soup.find('a', class_='productMainImage')['href']
+        return url
+    except:
+        append_to_logs(file_name, f'Error finding link {get_time()}\n')
+        append_to_logs(file_name, f'{soup}\n')
 
 
 def main(url, keyword):
@@ -41,26 +57,29 @@ def main(url, keyword):
         
         if res:
             soup = BeautifulSoup(res.content, 'html.parser')
-            products_list = soup.find(id='products')
-            products = products_list.find_all('li', class_='product')
-            idx, isAvailable, title = check_availability(products, keyword)
+            try:
+                products_list = soup.find(id='products')
+                products = products_list.find_all('li', class_='product')
+                idx, isAvailable, title = check_availability(products, keyword)
+            
+                if not isAvailable and hasSent:
+                    hasSent = False
 
-            if not isAvailable and hasSent:
-                hasSent = False
-
-            if isAvailable and not hasSent:
-                new_soup = products[idx]
-                product_id = new_soup['id'].strip('product-prod')
-                append_to_logs(file_name, f'Found item in stock check discord {get_time()}\n')
-                hasSent = True
-                image_url = get_image_url(new_soup)
-                price = get_price(new_soup)
-                link = get_link(new_soup)
-                send_webhook(link, 'Very: item in stock', title, image_url, price, product_id)
+                if isAvailable and not hasSent:
+                    new_soup = products[idx]
+                    product_id = new_soup['id'].strip('product-prod')
+                    append_to_logs(file_name, f'Found item in stock check discord {get_time()}\n')
+                    hasSent = True
+                    image_url = get_image_url(new_soup)
+                    price = get_price(new_soup)
+                    link = get_link(new_soup)
+                    send_webhook(link, 'Very: item in stock', title, image_url, price, product_id)
+            except:
+                append_to_logs(file_name, f'Error finding products {get_time()}\n')
+                append_to_logs(file_name, f'{soup}\n')
 
 
 if __name__ == '__main__':
     websites = get_info()[0]
     _url = websites['very_url']
-#    keyword = websites['very_keyword']
-    main(_url, 'playstation')
+    main(_url, 'console')
